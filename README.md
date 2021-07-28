db17c2340aade73d674806144345f5c8b654e5da
Floyd Jones <floyd-jones1@hotmail.com>
2021-07-27 23:22:27 -0400

    add readMe

--- README.md
+++ README.md
@@ -12,3 +12,38 @@ npm intall @tdotcode/react-lazy-retry
 // yarn
 yarn add @tdotcode/react-lazy-retry
 ```
+
+## Usage
+
+1. This will retry 5 times spaced out for 1 second between retries
+
+```javascript
+import React from "react";
+import lazyRetry from "@tdotcode/react-lazy-retry";
+const Greeting = lazyRetry(() => import("./components/Greeting"));
+
+function App() {
+  return (
+    <Suspense fallback={null}>
+      <Greeting />
+    </Suspense>
+  );
+}
+```
+
+2. You can also control number of retries and duration between retries.
+   In this example we are retrying 10 times spaced out for 100ms
+
+```javascript
+import React from "react";
+import lazyRetry from "@tdotcode/react-lazy-retry";
+const Greeting = lazyRetry(() => import("./components/Greeting"), 10, 100);
+
+function App() {
+  return (
+    <Suspense fallback={null}>
+      <Greeting />
+    </Suspense>
+  );
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,30 @@
-import { lazy, ComponentType, LazyExoticComponent } from 'react'
+import { ComponentType, lazy } from "react";
 
-type ComponentPromise<T = any> = Promise<{
-  default: T;
-}>
+type ComponentPromise<T = any> = Promise<{ default: ComponentType<T> }>;
 
-export default function lazyRetry<T extends ComponentType<any>>(component: Promise<ComponentPromise<T>>, retries?: number, interval?: number) : LazyExoticComponent<T> {
-  return lazy(() => retry<T>(component, retries, interval))
+export default function lazyRetry(
+  component: () => ComponentPromise,
+  retries?: number,
+  interval?: number
+): React.LazyExoticComponent<React.ComponentType<any>> {
+  return lazy(() => retry(component, retries, interval));
 }
 
-
-function retry<T>(component: Promise<ComponentPromise<T>>, retriesLeft = 5, interval = 1000): Promise<{
-  default: T;
-}>  {
+function retry(
+  fn: () => ComponentPromise,
+  retriesLeft = 5,
+  interval = 1000
+): ComponentPromise {
   return new Promise((resolve, reject) => {
-    component.then(resolve).catch((error) => {
+    fn()
+      .then(resolve)
+      .catch((error) => {
         setTimeout(() => {
           if (retriesLeft === 1) {
-            // reject('maximum retries exceeded');
             reject(error);
             return;
           }
-
-          // Passing on "reject" is the important part
-          retry(component, retriesLeft - 1, interval).then(resolve, reject);
+          retry(fn, retriesLeft - 1, interval).then(resolve, reject);
         }, interval);
       });
   });
